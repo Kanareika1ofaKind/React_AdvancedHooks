1174f6fdf9e7e73b7a13135cf8d6d675cb31638a
Sydney <proteimus@gmail.com>
2024-04-20 17:12:04 -0500

    input name, incrementation

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+function useInput(name, initialValue = '') {
+  const [value, setValue] = useLocalStorage(name, initialValue)
+  const onChange = e => setValue(e.target.value)
+  return { value, onChange, name }
+}
+
+function useLocalStorage(key, value) {
+  const [v, setV] = useState(() => {
+    const initial = localStorage.getItem(key)
+    return initial ? JSON.parse(initial) : value
+  })
+  const setValueLS = value => {
+    setV(value)
+    localStorage.setItem(key, JSON.stringify(value)) 
+  }
+  return [v, setValueLS]
+}
 
 export default function App() {
+  const inputProps = useInput('username')
+  const [count, setCount] = useLocalStorage('count', 1)
   return (
     <div>
-      <h2>Hello, World!</h2>
+      <input placeholder="Type things" {...inputProps} />
+      <div>Count is {count} 
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
     </div>
   )
 }
